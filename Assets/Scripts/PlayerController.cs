f31511e0cc5ace28507f15994671a80f96d747f5
Jorge Araya <jorgea1177@gmail.com>
2019-09-04 23:18:30 -0600

    Creación del repo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+
+    public float moveSpeed = 6;
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+
+    Controller2D controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Controller2D> ();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (Input.GetButtonDown("Jump") && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
