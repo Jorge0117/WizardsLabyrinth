60b9c9552aaf8425b6e1dd36bb36b379b9cd3a7c
Christofer <christofer.rodriguez@ucr.ac.cr>
2019-09-10 14:44:02 -0600

    Enemigo ve a chawa

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -14,23 +14,49 @@ public class EnemyController : MonoBehaviour
 
     public float y_impulso = 0;
 
+    public float distanciaVisivilidadChawa = 8;
+
+    float izq_visible;
+    float der_visible;
+
     float gravity;
     float jumpVelocity;
     Vector3 velocity;
 
     Controller2D controller;
+
+    GameObject jugador;
+
+    Transform enemigoTransform;
+
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<Controller2D> ();
+        jugador = GameObject.Find("Chawa");
+
+        enemigoTransform = GetComponent<Transform> ();
 
         gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
         jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+
+        izq_visible = distanciaVisivilidadChawa / -2;
+        der_visible = distanciaVisivilidadChawa / -2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("X: " + jugador.transform.position.x);
+        bool visibleChawa = false;
+        float distanciachawaenemigo = jugador.transform.position.x - enemigoTransform.position.x;
+
+
+        if( izq_visible <=  distanciachawaenemigo && der_visible <= distanciachawaenemigo){
+            visibleChawa = true;
+            Debug.Log("Lo estoy viendo");
+        }
+
         if (controller.collisions.above || controller.collisions.below)
         {
             velocity.y = 0.0f;
@@ -38,21 +64,36 @@ public class EnemyController : MonoBehaviour
 
         //Moverse random
         Vector2 impulso = new Vector2(Random.Range(-1.0f, 1f), 0.0f);
+        float anterior = this.x_impulso;
         this.x_impulso = impulso.x;
         this.y_impulso = impulso.y;
 
         // 25% de proba que intente cambiar la direccion para donde va
-        if(Random.Range(0f, 1f) > 0.75f){
-	        // 25% de proba que salte cuando el jugador presiona saltar, 10% de que salte por su cuenta
+        if(Random.Range(0f, 1f) > 0.75f && !visibleChawa){
+	        // 10% de proba que salte cuando el jugador presiona saltar, 10% de que salte por su cuenta
 	        if ( (Input.GetButtonDown("Jump") && controller.collisions.below && Random.Range(0f, 1f) < 0.10f) ||
 	        	 (controller.collisions.below && Random.Range(0f, 1f) < 0.10f) )
 	        {
 	            velocity.y = jumpVelocity;
 	        }
+
 	        velocity.x = impulso.x * moveSpeed;
 	        velocity.y += gravity * Time.deltaTime;
 	        controller.Move(velocity * Time.deltaTime);
 	    }else{
+	        if(visibleChawa){
+	            if( jugador.transform.position.x < enemigoTransform.position.x ){
+	                //mover hacia la izq
+	                impulso.x = Random.Range(-1.0f, 0f);
+	                velocity.x = impulso.x * moveSpeed;
+	                this.x_impulso = impulso.x;
+	            }else{
+	                //mover hacia la der
+	                impulso.x = Random.Range(0f, 1f);
+                    velocity.x = impulso.x * moveSpeed;
+                    this.x_impulso = impulso.x;
+	            }
+	        }
 	    	velocity.y += gravity * Time.deltaTime;
 	        controller.Move(velocity * Time.deltaTime);
 	    }
