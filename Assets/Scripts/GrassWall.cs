2ad768bf817780d5d831a41fc46779ff6a85cbe1
Estiven <estivenalg@gmail.com>
2019-09-11 21:02:18 -0600

    Avance graswall

--- Assets/Scripts/GrassWall.cs
+++ Assets/Scripts/GrassWall.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrassWall_ChangeSprite : MonoBehaviour
+public class GrassWall : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
