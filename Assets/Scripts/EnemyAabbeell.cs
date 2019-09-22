aa297c986f89a83fa27c7b55eb92ce9b452e59e3
Christofer <christofer.rodriguez@ucr.ac.cr>
2019-09-22 15:17:23 -0600

    Se separan los controladores de enemigos

---
+++ Assets/Scripts/EnemyAabbeell.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (EnemyController))]
+public class EnemyAabbeell : MonoBehaviour
+{
+    //Controlador de 2d
+    Controller2D controller;
+    
+    //Controlador de enemigo
+    EnemyController 
+    
+    
+    public float jumpHeight = 4;
+
+    public float timeToJumpApex = .4f;
+
+    public float moveSpeed = 6;
+
+    public float x_impulso = 0;
+
+    public float y_impulso = 0;
+
+    public float distanciaVisivilidadChawa = 8;
+
+    public float probaSaltarSiJugadorSalta = 0.1f;
+    public float probaSaltar = 0.05f;
+
+    public float probaCambiarDireccion = 0.25f;
+    public float probaCambiarDireccionChawaVisible = 0.1f;
+
+    float izq_visible;
+    float der_visible;
+
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+
+    
+
+    GameObject jugador;
+
+    Transform enemigoTransform;
+
+    
+    public float recoilVelocity = 0.9f;
+
+    int x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Controller2D> ();
+        jugador = GameObject.Find("Chawa");
+
+        enemigoTransform = GetComponent<Transform> ();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+
+        izq_visible = distanciaVisivilidadChawa / -2;
+        der_visible = distanciaVisivilidadChawa / -2;
+
+        x = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mover el enemigo cuando esta recibiendo daño, para simular que retrocede
+        if (isTakingDamage)
+        {
+            velocity.x = (jugador.transform.position.x < enemigoTransform.position.x ? recoilVelocity: recoilVelocity * -1)  * moveSpeed;
+        }
+        else
+        {
+            bool visibleChawa = false;
+            float distanciachawaenemigo = jugador.transform.position.x - enemigoTransform.position.x;
+
+            //Vemos si chawa esta visible
+            if (izq_visible <= distanciachawaenemigo && der_visible <= distanciachawaenemigo)
+            {
+                visibleChawa = true;
+            }
+            //Se quita velocidad y en caso de haber choque arriba o abajo
+            if (controller.collisions.above || controller.collisions.below)
+            {
+                velocity.y = 0.0f;
+            }
+            //Moverse random
+            Vector2 impulso = new Vector2(Random.Range(-1.0f, 1f), 0.0f);
+            float anterior = this.x_impulso;
+            this.x_impulso = impulso.x;
+            this.y_impulso = impulso.y;
+
+            // proba que salte cuando el jugador presiona saltar
+            // proba que salte por su cuenta
+            if ((Input.GetButtonDown("Jump") && controller.collisions.below &&
+                 Random.Range(0f, 1f) < probaSaltarSiJugadorSalta) ||
+                (controller.collisions.below && Random.Range(0f, 1f) < probaSaltar))
+            {
+                velocity.y = jumpVelocity;
+            }
+
+            // Proba de que intente cambiar la direccion sin chawa visible
+            // Proba de que intente cambiar la direccion con chawa visible
+            if ((Random.Range(0f, 1f) < probaCambiarDireccion && !visibleChawa) ||
+                (Random.Range(0f, 1f) < probaCambiarDireccionChawaVisible && visibleChawa))
+            {
+                velocity.x = impulso.x * moveSpeed;
+            }
+            else
+            {
+                if (visibleChawa)
+                {
+                    if (jugador.transform.position.x < enemigoTransform.position.x)
+                    {
+                        //mover hacia la izq
+                        impulso.x = Random.Range(-1.0f, 0f);
+                        velocity.x = impulso.x * moveSpeed;
+                        this.x_impulso = impulso.x;
+                    }
+                    else
+                    {
+                        //mover hacia la der
+                        impulso.x = Random.Range(0f, 1f);
+                        velocity.x = impulso.x * moveSpeed;
+                        this.x_impulso = impulso.x;
+                    }
+                }
+            }
+        }
+
+        //Realiza el movimiento
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
+        //Para girar el dibujo cada 10 frames
+        if ( x >= 10)
+        {
+            x = 0;
+            //girar el dibujo, SOLO  cuando no esta recibiendo daño
+            if ( !isTakingDamage && (velocity.x < 0 && enemigoTransform.localScale.x < 0 || velocity.x > 0 && enemigoTransform.localScale.x > 0) ){
+                enemigoTransform.localScale = new Vector3(enemigoTransform.localScale.x * -1, enemigoTransform.localScale.y, 1);                
+            }
+        }
+
+
+        x++;
+    }
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,148 +5,22 @@ using UnityEngine;
 [RequireComponent (typeof (Controller2D))]
 public class EnemyController : MonoBehaviour
 {
-    public float jumpHeight = 4;
-
-    public float timeToJumpApex = .4f;
-
-    public float moveSpeed = 6;
-
-    public float x_impulso = 0;
-
-    public float y_impulso = 0;
-
-    public float distanciaVisivilidadChawa = 8;
-
-    public float probaSaltarSiJugadorSalta = 0.1f;
-    public float probaSaltar = 0.05f;
-
-    public float probaCambiarDireccion = 0.25f;
-    public float probaCambiarDireccionChawaVisible = 0.1f;
-
-    float izq_visible;
-    float der_visible;
-
-    float gravity;
-    float jumpVelocity;
-    Vector3 velocity;
-
-    Controller2D controller;
-
-    GameObject jugador;
-
-    Transform enemigoTransform;
-
+    //Vida inicial de enemigo
     public int maxHealth = 6;
-    public float recoilVelocity = 0.9f;
+    
+    //Vida del enemigo
     private int currentHealth;
 
+    //Se esta recibiendo daño?
     private bool isTakingDamage = false;
+    
+    //Tiempo en que no se recibe mas daño
     public int invencibilitySeconds = 1;
-
-    int x;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<Controller2D> ();
-        jugador = GameObject.Find("Chawa");
-
-        enemigoTransform = GetComponent<Transform> ();
-
-        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-
-        izq_visible = distanciaVisivilidadChawa / -2;
-        der_visible = distanciaVisivilidadChawa / -2;
-
         currentHealth = maxHealth;
-
-        x = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Mover el enemigo cuando esta recibiendo daño, para simular que retrocede
-        if (isTakingDamage)
-        {
-            velocity.x = (jugador.transform.position.x < enemigoTransform.position.x ? recoilVelocity: recoilVelocity * -1)  * moveSpeed;
-        }
-        else
-        {
-            bool visibleChawa = false;
-            float distanciachawaenemigo = jugador.transform.position.x - enemigoTransform.position.x;
-
-            //Vemos si chawa esta visible
-            if (izq_visible <= distanciachawaenemigo && der_visible <= distanciachawaenemigo)
-            {
-                visibleChawa = true;
-            }
-            //Se quita velocidad y en caso de haber choque arriba o abajo
-            if (controller.collisions.above || controller.collisions.below)
-            {
-                velocity.y = 0.0f;
-            }
-            //Moverse random
-            Vector2 impulso = new Vector2(Random.Range(-1.0f, 1f), 0.0f);
-            float anterior = this.x_impulso;
-            this.x_impulso = impulso.x;
-            this.y_impulso = impulso.y;
-
-            // proba que salte cuando el jugador presiona saltar
-            // proba que salte por su cuenta
-            if ((Input.GetButtonDown("Jump") && controller.collisions.below &&
-                 Random.Range(0f, 1f) < probaSaltarSiJugadorSalta) ||
-                (controller.collisions.below && Random.Range(0f, 1f) < probaSaltar))
-            {
-                velocity.y = jumpVelocity;
-            }
-
-            // Proba de que intente cambiar la direccion sin chawa visible
-            // Proba de que intente cambiar la direccion con chawa visible
-            if ((Random.Range(0f, 1f) < probaCambiarDireccion && !visibleChawa) ||
-                (Random.Range(0f, 1f) < probaCambiarDireccionChawaVisible && visibleChawa))
-            {
-                velocity.x = impulso.x * moveSpeed;
-            }
-            else
-            {
-                if (visibleChawa)
-                {
-                    if (jugador.transform.position.x < enemigoTransform.position.x)
-                    {
-                        //mover hacia la izq
-                        impulso.x = Random.Range(-1.0f, 0f);
-                        velocity.x = impulso.x * moveSpeed;
-                        this.x_impulso = impulso.x;
-                    }
-                    else
-                    {
-                        //mover hacia la der
-                        impulso.x = Random.Range(0f, 1f);
-                        velocity.x = impulso.x * moveSpeed;
-                        this.x_impulso = impulso.x;
-                    }
-                }
-            }
-        }
-
-        //Realiza el movimiento
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-        
-        //Para girar el dibujo cada 10 frames
-        if ( x >= 10)
-        {
-            x = 0;
-            //girar el dibujo, SOLO  cuando no esta recibiendo daño
-            if ( !isTakingDamage && (velocity.x < 0 && enemigoTransform.localScale.x < 0 || velocity.x > 0 && enemigoTransform.localScale.x > 0) ){
-                enemigoTransform.localScale = new Vector3(enemigoTransform.localScale.x * -1, enemigoTransform.localScale.y, 1);                
-            }
-        }
-
-
-        x++;
     }
 
     public void takeDamage(int damage)
