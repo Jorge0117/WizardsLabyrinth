b568aa5dbcf69caa7855fb0027a04309af26249c
Jorge <jorgea1177@gmail.com>
2019-11-16 16:57:59 -0600

    Uff

--- Assets/Scripts/Control/MainMenu.cs
+++ Assets/Scripts/Control/MainMenu.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private MusicController musicController;
+
+    private void Awake()
+    {
+        musicController = GameObject.Find("Music Controller").GetComponent<MusicController>();
+    }
+
     public void NewGame()
     {
+        musicController.PlayMainTheme();
         PlayerPrefs.DeleteAll();
         SceneManager.LoadScene("Jungle");
     }
 
     public void ContinueGame()
     {
+        musicController.PlayMainTheme();
         string scene = PlayerPrefs.GetString("currentScene");
         SceneManager.LoadScene(scene);
     }
---
+++ Assets/Scripts/Control/MusicController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioClip mainTheme;
+
+    private static MusicController instance = null;
+    
+    public static MusicController Instance {
+        get { return instance; }
+    }
+
+    void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlayMainTheme()
+    {
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().clip = mainTheme;
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void Stop()
+    {
+        GetComponent<AudioSource>().Stop();
+    }
+}
